ab84037c2d08d52446f3fb9706eb78e4e668e0ed
zino <rhu5@u.rochester.edu>
2019-04-09 22:11:30 -0700

    Update apply action worker spec

--- spec/workers/delay_henka/apply_actions_worker_spec.rb
+++ spec/workers/delay_henka/apply_actions_worker_spec.rb
@@ -6,22 +6,24 @@ module DelayHenka
     describe '#perform' do
       let!(:actionable) { Foo.create(attr_chars: 'hello') }
 
-      it 'updates state of changes' do
-        record = ScheduledAction.create(actionable: actionable, submitted_by_id: 10, method_name: 'destroy', arguments: [], schedule_at: Time.current)
+      it 'executes action when schedule_at is in the past' do
+        ScheduledAction.schedule(record: actionable, by_id: 10, method_name: 'destroy', schedule_at: 1.hour.ago)
+        scheduled_action = ScheduledAction.last
 
         Sidekiq::Testing.inline! do
           expect{ described_class.perform_async }
-            .to change{ Foo.count }.from(1).to(0)
-            .and change{ record.reload.state }.from(DelayHenka::ScheduledChange::STATES[:STAGED]).to(DelayHenka::ScheduledChange::STATES[:COMPLETED])
+            .to change{ Foo.count }.by(-1)
+            .and change{ scheduled_action.reload.state }
+              .from(DelayHenka::ScheduledChange::STATES[:STAGED])
+              .to(DelayHenka::ScheduledChange::STATES[:COMPLETED])
         end
       end
 
-      it 'not updates until current time >= schedule_at' do
-        ScheduledAction.create(actionable: actionable, submitted_by_id: 10, method_name: 'destroy', arguments: [], schedule_at: 3.days.from_now)
+      it 'does not execute action if schedule_at is in the future' do
+        ScheduledAction.schedule(record: actionable, by_id: 10, method_name: 'destroy', schedule_at: 1.hour.from_now)
 
         Sidekiq::Testing.inline! do
-          expect{ described_class.perform_async }
-            .to_not change{ Foo.count }.from(1)
+          expect{ described_class.perform_async }.to_not change{ Foo.count }.from(1)
         end
       end
     end
