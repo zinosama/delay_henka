df463799b194883af5565d530b7c1ed10b9d107d
Kevin Voshell <kevin.voshell@chowbus.com>
2020-08-10 12:53:27 -0500

    move valid hours to constant, apply uniq to array of actions and changes

--- app/workers/delay_henka/updates_on_valid_time_zones_worker.rb
+++ app/workers/delay_henka/updates_on_valid_time_zones_worker.rb
@@ -2,9 +2,10 @@ module DelayHenka
   class UpdatesOnValidTimeZonesWorker
 
     include Sidekiq::Worker
+    VALID_HOURS = %w(1 2 3).freeze
 
     def perform
-      ScheduledAction.staged.distinct.pluck(:time_zone) + ScheduledChange.staged.distinct.pluck(:time_zone).each do |time_zone|
+      (ScheduledAction.staged.distinct.pluck(:time_zone) + ScheduledChange.staged.distinct.pluck(:time_zone)).uniq.each do |time_zone|
         if valid_scheduling_time?(time_zone)
           ApplyActionsWorker.perform_async(time_zone)
           ApplyChangesWorker.perform_async(time_zone)
@@ -16,8 +17,7 @@ module DelayHenka
 
     def valid_scheduling_time?(time_zone)
       return false unless Time.find_zone(time_zone)
-      valid_times = %w(1 2 3)
-      valid_times.include? Time.current.in_time_zone(time_zone).hour.to_s
+      VALID_HOURS.include? Time.current.in_time_zone(time_zone).hour.to_s
     end
   end
 end
